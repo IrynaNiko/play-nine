2502a32dac6e8c0468e68010cd74e6b4b859c753
Iryna <iryna.finogenova@gmail.com>
2018-10-24 21:35:42 -0500

    Adding Stars, Button and Answer components

--- game/src/js/client.js
+++ game/src/js/client.js
@@ -1,12 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import _ from 'lodash';
+
 import Game from "./components/Game";
+import Stars from "./components/Stars";
+import Button from "./components/Button";
+import Answer from "./components/Answer";
 
 class App extends React.Component { 
     render() {
       return (
         <div className="container">
             <Game />
+            <hr />
+            <div className="row">
+                <Stars />
+                <Button />
+                <Answer />
+            </div>
         </div>
       );
     }
